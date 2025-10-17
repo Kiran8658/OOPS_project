9ba6e878a789c646f834d3935cc10cbd44f1bec0
Kiran8658 <kittukumbhar2005@gmail.com>
2025-10-17 13:45:51 +0530

    add

--- src/api/userService.js
+++ src/api/userService.js
@@ -1,13 +1,14 @@
 import axios from "axios";
-
-const API_URL = import.meta.env.VITE_API_URL; // or process.env.REACT_APP_API_URL
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
+});
 
 export const getUsers = async () => {
-  const response = await axios.get(`${API_URL}/users`);
+  const response = await API.get("/users");
   return response.data;
 };
 
 export const addUser = async (user) => {
-  const response = await axios.post(`${API_URL}/users`, user);
+  const response = await API.post("/users", user);
   return response.data;
 };
