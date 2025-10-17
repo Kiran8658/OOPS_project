c53c0d0a0d8d43a2b9cc6a987ecc84b4a3ea80ef
Kiran8658 <kittukumbhar2005@gmail.com>
2025-10-17 13:45:42 +0530

    add

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-
 interface LoginProps {
   onLogin: () => void;
 }
